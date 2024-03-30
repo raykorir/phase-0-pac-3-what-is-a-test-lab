53c84c835c77f124fa77bfa60bc9d48cb5216d97
raymond <raymond.korir@student.moringaschool.com>
2024-03-30 14:03:53 +0300

    work  complete

--- test/indexTest.js
+++ test/indexTest.js
@@ -10,7 +10,7 @@ describe("what-is-a-test", () => {
   });
 
   describe("Height", () => {
-    it("is less than 40 and greater than 0", () => {
+    it(" 39 is less than 40 and greater than 0", () => {
       expect(height).toBeMoreThan(0)
       expect(height).toBeLessThan(40);
     });
